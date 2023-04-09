60eb993c3cd097081f6f57dd8200dfc3481986e1
ckomalram <glaw14@gmail.com>
2023-04-09 11:46:08 -0500

    ++ init + entity + context + cns + migration

--- Servicio.api.Auth/Program.cs
+++ Servicio.api.Auth/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Servicio.api.Auth.Core.Context;
+using Servicio.api.Auth.Core.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +13,47 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//conf Cors
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsRule", rule =>
+    {
+        // rule.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://mipagina.com");
+        rule.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+    });
+});
+
+// conf sql server
+builder.Services.AddSqlServer<SecurityContext>(builder.Configuration.GetConnectionString("cnSecurityDb"));
+
+// conf identity core: autenticacion y autorización
+builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<SecurityContext>().AddSignInManager<SignInManager<User>>();
+
+// conf system clock: forma de obtener la hora actual y se utiliza en la autenticación de ASP.NET Core, en particular para validar los tokens JWT
+builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
 var app = builder.Build();
+
+// conf user for testing
+using (var context = app.Services.CreateScope())
+{
+    var services = context.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var contextEf = services.GetRequiredService<SecurityContext>();
+
+        SecurityContextData.InsertUser(contextEf, userManager).Wait();
+
+    }
+    catch (Exception e)
+    {
+        var logging = services.GetRequiredService<ILogger<Program>>();
+        logging.LogError(e, "Error al registrar usuario de prueba");
+    }
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +63,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("CorsRule");
 
 app.UseAuthorization();
 
--- Servicio.api.Auth/info.md
+++ Servicio.api.Auth/info.md
@@ -1,4 +1,16 @@
 # 1.0.1
 
-Crear proyecto
-Instalar librerías
+- Crear proyecto
+- Instalar librerías
+- Crear entidad User
+- Crear contexto de seguridad
+- Crear ConnectionString en appsettings.json
+- Se inyecta sql server en program cs
+- Se inyecta iodentity core en program cs
+- Se inyecta SystemClock para hora actual en programcs.
+- Se crea securitycontextData
+- Se inyecta securitycontextdata en program.cs
+- Crear migracion
+  - dotnet ef migrations add InitialCreate
+- Ejecutar Migracion
+  - dotnet ef database update
